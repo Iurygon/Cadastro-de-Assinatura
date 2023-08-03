22632d0e64acf127385d59aa391a33213df961bd
Iury Gontijo Silva <115959057+Iurygon@users.noreply.github.com>
2023-08-02 23:20:51 -0300

    Adição do script

---
+++ script.js
@@ -0,0 +1,11 @@
+let campo_nome = document.getElementById('nome');
+let campo_nome_erro = document.getElementById('erro_nome');
+
+let campo_sobrenome = document.getElementById('sobrenome');
+let campo_sobrenome_erro = document.getElementById('erro_sobrenome');
+
+let campo_email = document.getElementById('email');
+let campo_email_erro = document.getElementById('erro_email');
+
+let campo_senha = document.getElementById('senha');
+let campo_senha_erro = document.getElementById('erro_senha');
