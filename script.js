ac1bb36a51b7307ae8c2737e9d5f238564c6ba06
Iury Gontijo Silva <115959057+Iurygon@users.noreply.github.com>
2023-08-22 23:11:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 let form = document.getElementById('formulario');
 let inputs = document.querySelectorAll('.campo');
+let erro = document.querySelectorAll('.erro');
+let emailCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
