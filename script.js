728495fc1b13c13f10734af1b923e16e687431f8
Iury Gontijo Silva <115959057+Iurygon@users.noreply.github.com>
2023-08-23 23:38:27 -0300

    Adição de JS

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ let form = document.getElementById('formulario');
 let inputs = document.querySelectorAll('.campo');
 let erro = document.querySelectorAll('.erro');
 let emailCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+
+function nameValidate(){
+
+}
+
+function lastNameValidate(){
+
+}
+
+function emailValidate(){
+
+}
+
+function passwordValidate(){
+
+}
