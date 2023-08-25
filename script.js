5436096792ef5eba22c46811ae84856e9129567c
Iury Gontijo Silva <115959057+Iurygon@users.noreply.github.com>
2023-08-24 22:45:27 -0300

    Atualização em HTML, CSS e JS

--- script.js
+++ script.js
@@ -1,22 +1,74 @@
-let form = document.getElementById('formulario');
-let inputs = document.querySelectorAll('.campo');
-let erro = document.querySelectorAll('.erro');
-let emailCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+/*RECEBE ELEMENTOS*/
 
+const form   = document.getElementById('formulario');
+const inputs = document.querySelectorAll('.campo');
+const erro   = document.querySelectorAll('.erro');
+const emailCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
+/* VALIDAÇÃO */
 
-function nameValidate(){
+form.addEventListener('submit', (event) =>{
+    event.preventDefault();
+    nameValidate();
+    emailValidate();
+    passValidate();
+    comparaSenha();
+});
 
+function invalido(index){
+    inputs[index].style.border = '2px solid #fc1c1c';
+    inputs[index].style.backgroundImage = '';
+    erro[index].style.display = 'block';
 }
 
-function lastNameValidate(){
+function valido(index){
+    inputs[index].style.border = '';
+    inputs[index].style.background = 'none';
+    erro[index].style.display = 'none';
+}
 
+/*TESTES*/
+
+function nameValidate(){
+    if(inputs[0].value.length <= 2)
+    {
+        invalido(0);
+    }
+    else
+    {
+        valido(0);
+    }
 }
 
 function emailValidate(){
-
+    if(!emailCheck.test(inputs[1].value))
+    {
+        invalido(1);
+    }
+    else
+    {
+        valido(1);
+    }
 }
 
-function passwordValidate(){
+function passValidate(){
+    if(inputs[2].value.length < 8)
+    {
+        invalido(2);
+    }
+    else
+    {
+        valido(2);
+    }
+}
 
+function comparaSenha(){
+    if(inputs[2].value != inputs[3].value || inputs[3].value.length < 8)
+    {
+        invalido(3);
+    }
+    else
+    {
+        valido(3);
+    }
 }
